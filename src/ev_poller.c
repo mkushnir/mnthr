33345329687549673617edeb6dcceadf6e2e5426
Markiyan Kushnir <markiyan.kushnir@gmail.com>
2016-04-19 23:52:26 +0300

    use BDATA() macro

--- src/ev_poller.c
+++ src/ev_poller.c
@@ -205,7 +205,7 @@ ev_item_new_stat(const char *path, UNUSED int event)
     BYTES_INCREF(res->stat_path);
     ev_stat_init(&res->ev.stat,
                  ev_stat_cb,
-                 res->stat_path->data, 0.0);
+                 BDATA(res->stat_path), 0.0);
     res->ev.stat.data = NULL;
     res->hash = 0;
     res->ty = EV_TYPE_STAT;
