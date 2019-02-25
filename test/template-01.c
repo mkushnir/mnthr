9cb0142ec6b2bc5b2862f0fc61be14e42894fd31
Markiyan Kushnir [at home] <markiyan.kushnir@gmail.com>
2019-02-25 17:48:16 +0200

    move aucotonf to .ac-aux

--- test/template-01.c
+++ test/template-01.c
@@ -155,7 +155,7 @@ main(int argc, char **argv)
             exit(0);
 
         case 'V':
-            printf("%s\n", VERSION);
+            printf("%s\n", PACKAGE_STRING);
             exit(0);
 
         case 'f':
